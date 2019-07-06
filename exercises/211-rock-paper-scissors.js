6fa65d92b63637616f713cb6fc922bb7063d0f10
Alvin Ng <alvinng@Gundam.local>
2019-07-05 23:09:57 -0500

    attempt at rock paper scissors

--- exercises/211-rock-paper-scissors.js
+++ exercises/211-rock-paper-scissors.js
@@ -6,3 +6,20 @@
 // rockPaperScissors('rock', 'scissors') --> 'player 1'
 // rockPaperScissors('rock', 'paper') --> 'player 2'
 // rockPaperScissors('paper', 'paper') --> 'draw'
+function rockPaperScissors (throwOne, throwTwo) {
+let itemOne = {
+    r: 'rock',
+    p: 'paper',
+    s: 'scissor'
+}
+let itemTwo = {
+    r: 'rock',
+    p: 'paper',
+    s: 'scissor'
+}
+if (throwOne === itemOne) {
+    return 'draw'
+} else if (throwOne === throwOne.r && throwTwo === throwTwo.p || throwTwo.s) {
+    return 'player 1'
+}
+}
