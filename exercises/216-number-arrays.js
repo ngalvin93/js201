61d9f8758e93733b0663f1c74c9b5617153acbce
Alvin Ng <alvinng@Gundam.local>
2019-07-08 20:44:02 -0500

    completed number arrays

--- exercises/216-number-arrays.js
+++ exercises/216-number-arrays.js
@@ -26,25 +26,25 @@ function sumNumbers (arrayOfNumbers) {
 // positives([1, 2, 3]) --> [1, 2, 3]
 // positives([-1, -2, -3]) --> []
 function positives (arrayOfNumbers) {
-
+  let newArrayOfNumbers = arrayOfNumbers.filter(val => val > 0);
+  return newArrayOfNumbers
 }
-
-
-
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "evens" which takes an array of numbers and returns a new
 // array containing only the even numbers in the given array.
 // Hint: you may want to re-use your "isEven" function from 01-predicate-functions.js
-
-
-
+function evens (arrayOfNumbers) {
+  let newArrayOfNumbers = arrayOfNumbers.filter(val => val % 2 === 0);
+  return newArrayOfNumbers
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "odds" which takes an array of numbers and returns a new
 // array containing only the odd numbers in the given array.
 // Hint: you may want to re-use your "isOdd" function from 01-predicate-functions.js
-
-
-
+function odds (arrayOfNumbers) {
+  let newArrayOfNumbers = arrayOfNumbers.filter(val => val % 2 !== 0);
+  return newArrayOfNumbers
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "integers" which takes an array of numbers and returns a new
 // array containing only the integers in the given array.
@@ -52,12 +52,21 @@ function positives (arrayOfNumbers) {
 //
 // Example:
 // integers([3.14, 2.4, 7, 8.1, 2]) --> [7, 2]
-
-
-
+function integers (arrayOfNumbers) {
+  let newArrayOfNumbers = [];
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    if (Number.isInteger(arrayOfNumbers[i]) === true) {
+      newArrayOfNumbers.push(arrayOfNumbers[i]);
+    }
+  }
+  return newArrayOfNumbers
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "squareDance" which takes an array of numbers and returns a
 // new array containing the result of squaring each of the numbers in the given array.
 //
 // Example:
 // squareDance([1, 2, 3]) --> [1, 4, 9]
+function squareDance (arrayOfNumbers) {
+  return arrayOfNumbers.map(x => x * x)
+}
