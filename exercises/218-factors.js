ba0c152e1496b6b42bb55238d69e7e937af546ff
Alvin Ng <alvinng@Gundam.local>
2019-07-08 22:44:56 -0500

    completed factors

--- exercises/218-factors.js
+++ exercises/218-factors.js
@@ -6,9 +6,14 @@
 // gcd(5, 1) --> 1
 // gcd(3, 15) --> 3
 // gcd(50, 20) --> 10
-
-
-
+function gcd (x, y) {
+    while (y) {
+        var t = y;
+        y = x % y;
+        x = t;
+      }
+      return x;
+}
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "factors" which is given a number and returns an array
 // containing all its factors.
@@ -18,4 +23,12 @@
 // factors(1) --> [1]
 // factors(12) --> [1, 2, 3, 4, 6, 12]
 // factors(42) --> [1, 2, 3, 6, 7, 14, 21, 42]
-
+function factors (number) {
+    let arrayOfFactors = [];
+    for ( i=1; i<=number; i++) {
+        if (number%i === 0) {
+            arrayOfFactors.push(i)
+        }
+    }
+    return arrayOfFactors
+}
