982c4feecf810507173463b520b5cad930ecfc23
Alvin Ng <alvinng@Gundam.local>
2019-07-13 00:03:33 -0500

    completed leetspeak

--- exercises/235-leetspeak.js
+++ exercises/235-leetspeak.js
@@ -14,3 +14,38 @@
 // Examples:
 // leetspeak('Leet') --> "l337"
 // leetspeak('ORANGE') --> "0r4n63"
+function leetspeak (string) {
+let leetAlpha = {
+    A : '4',
+    B : 'B',
+    C : 'C',
+    D : 'D',
+    E : '3',
+    F : 'F',
+    G : '6',
+    H : 'H',
+    I : '1',
+    J : 'J',
+    K : 'K',
+    L : 'L',
+    M : 'M',
+    N : 'N',
+    O : '0',
+    P : 'P',
+    Q : 'Q',
+    R : 'R',
+    S : '5',
+    T : '7',
+    U : 'U',
+    V : 'V',
+    W : 'W',
+    X : 'X',
+    Y : 'Y',
+    Z : 'Z'
+}
+let stringArr = string.toUpperCase().split('')
+for (let i in stringArr) {
+    stringArr[i] = leetAlpha[stringArr[i]]
+}
+return stringArr.join('').toLowerCase()
+}
